d9676358b2d648a3258c0f05962becce7c831ca3
rajatmanitripathi <rajat07102000@gmail.com>
2024-08-19 13:36:54 +0530

    UI changes

--- src/app/custom-progress-bar/custom-progress-bar.component.ts
+++ src/app/custom-progress-bar/custom-progress-bar.component.ts
@@ -7,5 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 export class CustomProgressBarComponent {
   @Input() value: number = 0;
-  @Input() color: string = '#3f51b5';
+  @Input() color: string = '#e6281d';
+  @Input() borderColor: string = '#ffcc00';
+
+  // maxStars: number = 5;
+  // stars: number[] = Array(this.maxStars).fill(0);
+  // filledStars!: number;
+  // halfStar!: boolean;
+
+  // ngOnInit() {
+  //   this.calculateStars();
+  // }
+
+  // calculateStars() {
+  //   const starsValue = (this.value / 100) * this.maxStars;
+  //   this.filledStars = Math.floor(starsValue);
+  //   this.halfStar = starsValue % 1 !== 0;
+  // }
 }
