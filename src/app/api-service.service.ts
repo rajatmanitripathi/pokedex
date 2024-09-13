32431364d6b27e7bf48f70811d2dca6bc8945e53
rajatmanitripathi <rajat07102000@gmail.com>
2024-09-13 12:52:18 +0530

    index db storage

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -9,57 +9,158 @@ import {
   specieDetails,
 } from './models/pokemonAPIModels';
 import { SpinnerLoaderService } from './spinner-loader.service';
+import { evolutionChain } from './models/evoltionChain';
+import moment from 'moment';
+import pako from 'pako';
 
 @Injectable()
 export class ApiServiceService {
+  expiryDuration:number = 432000; //5 days
   pokemon: BehaviorSubject<Pokemon | null> = new BehaviorSubject<Pokemon | null>(null);
-  constructor(private httpClient: HttpClient, private loaderService:SpinnerLoaderService) {
+  constructor(private httpClient: HttpClient, private loaderService: SpinnerLoaderService) {
 
   }
- 
+
   getPokemonDataList(): BehaviorSubject<Pokemon | null> {
     this.loaderService.showLoader();
-    this.getAllPokemonList().subscribe(async (res) => {
+    this.getPokemonDataFromLocalDB().then(res => {
+      let pokemonDataFromLocalDB: Pokemon | null = res;
+      if (pokemonDataFromLocalDB) {
+        this.pokemon.next(pokemonDataFromLocalDB);
+        this.loaderService.hideLoader();
+      }
+      else {
+        this.updatePokemonDataListFromApi();
+      }
+    },
+      rej => {
+        this.updatePokemonDataListFromApi();
+      }).catch(err => {
+        this.updatePokemonDataListFromApi();
+      });
 
+    return this.pokemon;
+  }
+
+  updatePokemonDataListFromApi() {
+    this.getAllPokemonList().subscribe(async (res) => {
       if (res?.results) {
         // Map through all the Pokémon and get their data
         const pokemonPromises = res.results.map(async (pokemon: PokemonInfo) => {
           const pokemonDataUrl = pokemon?.url;
-  
+
           const pokemonData = await this.getPokemonData(pokemonDataUrl).toPromise();
           if (pokemonData?.species?.url) {
             pokemon.pokemonData = pokemonData;
-  
+
             const speciesUrl = pokemonData?.species?.url;
             const speciesData = await this.getSpeciesData(speciesUrl).toPromise();
-  
+
             pokemon.pokemonData.specieData = speciesData;
           }
         });
-  
+
         // Wait for all promises to complete
         await Promise.all(pokemonPromises);
-  
+
         // Once all data is loaded, update the BehaviorSubject
         this.shuffle(res.results);
         this.pokemon.next(res);
-        this.loaderService.hideLoader()
+        this.setPokemonDataInLocalDB(res, 432000);
+        this.loaderService.hideLoader();
       }
     });
-    
-    return this.pokemon;
   }
 
-  shuffle(array:any[]) {
+  openDatabase(): Promise<IDBDatabase> {
+    return new Promise((resolve, reject) => {
+      let request = indexedDB.open("pokemonDatabase", 1);
+
+      request.onupgradeneeded = function (event) {
+        let db = request.result;
+        if (!db.objectStoreNames.contains("pokemonDataStore")) {
+          db.createObjectStore("pokemonDataStore", { keyPath: "id" });
+        }
+      };
+
+      request.onsuccess = function () {
+        resolve(request.result);
+      };
+
+      request.onerror = function (event) {
+        reject(event);
+      };
+    });
+  }
+
+
+  async setItemInLocalDB(key: string, value: any, createdAt: Date, expireAt: Date | null = null): Promise<void> {
+    let db = await this.openDatabase();
+    let dataToStore: { createdAt: Date, expireAt: Date | null, data: Pokemon } = { createdAt: createdAt, expireAt: expireAt, data: value };
+
+    return new Promise((resolve, reject) => {
+      let transaction = db.transaction("pokemonDataStore", "readwrite");
+      let objectStore = transaction.objectStore("pokemonDataStore");
+      let request = objectStore.put({ id: key, data: dataToStore }); // Using 'key' as the ID
+
+      request.onsuccess = () => resolve();
+      request.onerror = (event) => reject(event);
+    });
+  }
+
+  async getItemFromLocalDB(key: string): Promise<Pokemon> {
+    let db = await this.openDatabase();
+
+    return new Promise((resolve, reject) => {
+      let transaction = db.transaction("pokemonDataStore", "readonly");
+      let objectStore = transaction.objectStore("pokemonDataStore");
+      let request = objectStore.get(key);
+
+      request.onsuccess = () => {
+        if (request?.result?.data && request.result.data.expireAt && moment().diff(request.result.data.expireAt, "seconds") > 0) {
+          reject("Data expired!");
+        }
+        else {
+          resolve(request.result?.data?.data ?? null);
+        }
+      }
+      request.onerror = (event) => reject(event);
+    });
+  }
+
+  async setPokemonDataInLocalDB(pokemonData: Pokemon, expiryDurationInSecs?: number): Promise<void> {
+    let createdAt: Date = moment().toDate();
+    let expireAt: Date | null = expiryDurationInSecs ? moment(createdAt).add(expiryDurationInSecs, "seconds").toDate() : null;
+    return this.setItemInLocalDB("pokemonData", pokemonData, createdAt, expireAt);
+  }
+
+  async getPokemonDataFromLocalDB(): Promise<Pokemon> {
+    return this.getItemFromLocalDB("pokemonData");
+  }
+
+  compressData(data: any): Uint8Array {
+    const jsonString = JSON.stringify(data);
+    const compressedData = pako.deflate(jsonString);
+    return compressedData;
+  }
+
+  decompressData(compressedData: Uint8Array): any {
+    const decompressedJsonString = pako.inflate(compressedData, { to: 'string' });
+    const originalData = JSON.parse(decompressedJsonString);
+    return originalData;
+  }
+
+
+  shuffle(array: any[]) {
     let currentIndex = array.length;
-  
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
+
       // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
@@ -68,12 +169,12 @@ export class ApiServiceService {
 
   getAllPokemonPaginatedList(offset: number, limit: number = 12): Observable<Pokemon> {
     return this.httpClient.get<Pokemon>(
-       `${apiConstant.pokeApiBaseUrl}pokemon?limit=${limit}&offset=${offset}`
+      `${apiConstant.pokeApiBaseUrl}pokemon?limit=${limit}&offset=${offset}`
     );
   }
   getAllPokemonList(): Observable<Pokemon> {
     return this.httpClient.get<Pokemon>(
-       apiConstant.pokeApiBaseUrl+apiConstant.pokemonList+"?limit=100000&offset=0"
+      apiConstant.pokeApiBaseUrl + apiConstant.pokemonList + "?limit=100000&offset=0"
     );
   }
   getPokemonData(url: string): Observable<PokemonDetails> {
@@ -81,5 +182,8 @@ export class ApiServiceService {
   }
   getSpeciesData(specieUrl: string): Observable<specieDetails> {
     return this.httpClient.get<specieDetails>(specieUrl);
+  }
+  getEvoChain(evoUrl: string): Observable<evolutionChain> {
+    return this.httpClient.get<evolutionChain>(evoUrl);
   }
 }
--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject, Input } from '@angular/core';
 
 
 import { child, evolutionChain, TreeNode } from '../models/evoltionChain';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PokemonInfo } from '../models/pokemonAPIModels';
+import { ApiServiceService } from '../api-service.service';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -9,501 +12,17 @@ import { child, evolutionChain, TreeNode } from '../models/evoltionChain';
   styleUrl: './pokemon-details.component.scss',
 })
 export class PokemonDetailsComponent {
-  @Input() chain: evolutionChain = {
-    "baby_trigger_item": null,
-    "chain": {
-      "evolution_details": [],
-      "evolves_to": [
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": {
-                "name": "water-stone",
-                "url": "https://pokeapi.co/api/v2/item/84/"
-              },
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "use-item",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/3/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "vaporeon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/134/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": {
-                "name": "thunder-stone",
-                "url": "https://pokeapi.co/api/v2/item/83/"
-              },
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "use-item",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/3/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "jolteon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/135/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": {
-                "name": "fire-stone",
-                "url": "https://pokeapi.co/api/v2/item/82/"
-              },
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "use-item",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/3/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "flareon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/136/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": 160,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "day",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "espeon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/196/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": 160,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "night",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "umbreon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/197/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "eterna-forest",
-                "url": "https://pokeapi.co/api/v2/location/8/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "pinwheel-forest",
-                "url": "https://pokeapi.co/api/v2/location/375/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "kalos-route-20",
-                "url": "https://pokeapi.co/api/v2/location/650/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": {
-                "name": "leaf-stone",
-                "url": "https://pokeapi.co/api/v2/item/85/"
-              },
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "use-item",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/3/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "leafeon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/470/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "sinnoh-route-217",
-                "url": "https://pokeapi.co/api/v2/location/48/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "twist-mountain",
-                "url": "https://pokeapi.co/api/v2/location/380/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": null,
-              "location": {
-                "name": "frost-cavern",
-                "url": "https://pokeapi.co/api/v2/location/640/"
-              },
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": {
-                "name": "ice-stone",
-                "url": "https://pokeapi.co/api/v2/item/885/"
-              },
-              "known_move": null,
-              "known_move_type": null,
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "use-item",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/3/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "glaceon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/471/"
-          }
-        },
-        {
-          "evolution_details": [
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": {
-                "name": "fairy",
-                "url": "https://pokeapi.co/api/v2/type/18/"
-              },
-              "location": null,
-              "min_affection": 2,
-              "min_beauty": null,
-              "min_happiness": null,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            },
-            {
-              "gender": null,
-              "held_item": null,
-              "item": null,
-              "known_move": null,
-              "known_move_type": {
-                "name": "fairy",
-                "url": "https://pokeapi.co/api/v2/type/18/"
-              },
-              "location": null,
-              "min_affection": null,
-              "min_beauty": null,
-              "min_happiness": 160,
-              "min_level": null,
-              "needs_overworld_rain": false,
-              "party_species": null,
-              "party_type": null,
-              "relative_physical_stats": null,
-              "time_of_day": "",
-              "trade_species": null,
-              "trigger": {
-                "name": "level-up",
-                "url": "https://pokeapi.co/api/v2/evolution-trigger/1/"
-              },
-              "turn_upside_down": false
-            }
-          ],
-          "evolves_to": [],
-          "is_baby": false,
-          "species": {
-            "name": "sylveon",
-            "url": "https://pokeapi.co/api/v2/pokemon-species/700/"
-          }
-        }
-      ],
-      "is_baby": false,
-      "species": {
-        "name": "eevee",
-        "url": "https://pokeapi.co/api/v2/pokemon-species/133/"
-      }
-    },
-    "id": 67
+  chain!: evolutionChain
+
+
+  modifiedChain: TreeNode[] = []
+
+
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { data: PokemonInfo }, private apiService: ApiServiceService) {
   }
-  
 
- modifiedChain:TreeNode[]=[]
- 
-
-  rootNode = {
-    text: {
-      name: 'ROOT'
-    },
-    HTMLclass: 'angular',
-    HTMLid: 'tree-root',
-    id: 0,
-    image: '/assets/images/Bug.jpg',
-  };
-  newEvolves_to: any[] = [];
-  constructor() { }
-   convertEvolutionChainToTreeNode(chain: evolutionChain): TreeNode[] {
+  convertEvolutionChainToTreeNode(chain: evolutionChain): TreeNode[] {
     // Recursive function to process each evolution
     function processEvolution(evolution: child): TreeNode {
       const id = getIdFromUrl(evolution.species.url);
@@ -511,16 +30,16 @@ export class PokemonDetailsComponent {
         label: capitalizeFirstLetter(evolution.species.name),
         data: evolution.species.name,
         expanded: true, // You can adjust this based on your needs
-        image:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${id}.gif`
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${id}.gif`
       };
-  
+
       if (evolution.evolves_to && evolution.evolves_to.length > 0) {
         node.children = evolution.evolves_to.map((evol: child) => processEvolution(evol));
       }
-  
+
       return node;
     }
-  
+
     // Helper function to capitalize the first letter of a Pokémon's name
     function capitalizeFirstLetter(name: string): string {
       return name.charAt(0).toUpperCase() + name.slice(1);
@@ -529,7 +48,7 @@ export class PokemonDetailsComponent {
       const parts = url.split('/');
       return parts[parts.length - 2]; // ID is the second-to-last part of the URL
     }
-  
+
     // Start processing from the root of the chain
     this.modifiedChain.push(processEvolution(chain.chain))
     console.log(this.modifiedChain)
@@ -538,8 +57,16 @@ export class PokemonDetailsComponent {
 
 
   ngOnInit() {
-   this.convertEvolutionChainToTreeNode(this.chain);
+    if (this.data?.data?.pokemonData?.specieData?.evolution_chain.url) {
+      this.apiService.getEvoChain(this.data?.data?.pokemonData?.specieData?.evolution_chain.url).subscribe((res) => {
+        this.chain = res;
+
+        this.convertEvolutionChainToTreeNode(this.chain);
+      })
+
+    }
+
   }
-  
-  
+
+
 }
